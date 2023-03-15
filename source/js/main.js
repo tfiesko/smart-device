ac211ab8e619a31caba9d68c8664dd828ccfa84e
Anton Tarasenko <report641@gmail.com>
2023-03-15 21:58:55 +0700

    Завершен футер

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initAccordions} from './vendor/accordion/init-accordion';
+import {accordions} from './vendor/accordion/init-accordion';
 
 // ---------------------------------
 
@@ -21,6 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    initAccordions();
   });
 });
 
@@ -48,3 +51,4 @@ window.addEventListener('DOMContentLoaded', () => {
 // breakpointChecker();
 
 // используйте .closest(el)
+
